0d6bf9b944f39252bc31bef9cdf6de6dd8b6e6d7
amp167 <aungmyo16718@gmail.com>
2023-11-16 21:49:46 +0630

    first

---
+++ Chapter-2/PHP_4_Accessing_class_methods.php
@@ -0,0 +1,8 @@
+<?php
+class PHP_4_Accessing_class_methods{
+    function greeting(){
+        echo "hello from AMP TECHNOLOGY";
+    }
+}
+$obj = new PHP_4_Accessing_class_methods();
+$obj->greeting();
---
+++ Chapter-2/PHP_Method_overloading.php
@@ -0,0 +1,4 @@
+<?php
+class PHP_Method_overloading{
+    
+}
